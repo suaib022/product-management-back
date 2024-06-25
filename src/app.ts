268044b0b4663de4aeb9aa220b3a05dc36bd2ef1
suaib022 <suaib.109206@gmail.com>
2024-06-25 23:09:29 +0600

    19th commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { ProductRoutes } from './app/modules/products/productRoute';
 import { orderRoutes } from './app/modules/orders/orderRoute';
+
 const app: Application = express();
 
 app.use(express.json());
@@ -9,21 +10,21 @@ app.use(cors());
 
 // application routes
 app.use('/api/products', ProductRoutes);
-
 app.use('/api/orders', orderRoutes);
 
-app.get('/', (req: Request, res: Response) => {
+app.use('/', (req: Request, res: Response) => {
   res.send('Welcome to the Home Page!');
 });
 
-//Error Handling
+// Error Handling
 app.use((req: Request, res: Response) => {
   res.status(404).json({
     success: false,
     message: 'Route Not Found!',
   });
 });
-app.use((err: any, req: Request, res: Response) => {
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.status || 500).json({
     success: false,
     message: err.message || 'Server Error!',
